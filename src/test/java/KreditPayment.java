82b2415824a294939223e1366efb3d44d256d2f3
A_Artamonov <artamonov2@gmail.com>
2020-11-27 09:20:18 +0200

    Some changes to test / commit push process!

--- src/test/java/KreditPayment.java
+++ src/test/java/KreditPayment.java
@@ -5,26 +5,24 @@ public class KreditPayment {
         @Test
         public void Credit() {
 
-            int a = 956872; // Credit amount.
-            int b = 4;     // Interest rate.
+            double a = 956872; // Credit amount.
+            double b = 4.5;     //  Interest rate.
 
             System.out.println("Body of the credit: " +a +"$");
 
-
-                    int percent = percentage(a, b);
+                    double percent = percentage(a, b);
                         System.out.println("Amount of percent for the credit: " + percent + "$");
 
-
-                    int sum = total(percent,a);
+                    double  sum = total(percent,a);
                         System.out.println("Total return amount: " +sum + "$");
         }
-        public int percentage(int a, int b){
-            int x = ((a/100)*b);
+        public double percentage(double a, double b){
+            double x = ((a/100)*b);
             return x;
         }
-
-        public int total(int a, int x){
-            int z = a + x;
+        public double total(double a, double x){
+            double z = a + x;
             return z;
+
         }
 }
